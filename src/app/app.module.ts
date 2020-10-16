effff9e932c6d4c3181540cc7cf6e28656a1b22e
AndrewPru <58787232+AndrewPru@users.noreply.github.com>
2020-10-16 20:41:44 +0300

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { SelectorComponent } from './selector/selector.component';
+import { OrganizerComponent } from './organizer/organizer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganizerComponent
   ],
   imports: [
     BrowserModule
